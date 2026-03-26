5f46f317474bf233df56a806c5e188f10d842cb4
iyyneswaran <pdee5660@gmail.com>
2026-03-27 00:49:12 +0530

    PWA fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c84425d6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vt1m136f81"
+    "revision": "0.rq26b23saog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
